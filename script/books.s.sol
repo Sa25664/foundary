410e0983c2512cc0b421f99606d08202046ec246
Sa25664 <sakollu@my.bridgeport.edu>
2025-04-21 13:11:38 -0500

    Add files via upload

---
+++ script/books.s.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import {Script} from "forge-std/Script.sol";
+import {Books} from "../src/Books.sol";
+
+contract Deploy is Script {
+    Books public books;
+
+    function run() public {
+        books = new Books("Programming Foundry", "Pierre Tidou", 100);
+      
+    }
+}
---
+++ test/Books.t.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import "../src/Books.sol";
+
+contract BooksTest is Test {
+    Books public books;
+    Books.Book public book;
+
+    function setUp() public {
+     
+        books = new Books("Programming Foundry", "Pierre Tidou", 100);
+        console.log("Books contract deployed at address:", address(books));
+    }
+
+    function test_true() public pure {
+     
+        assert(true);
+    }
+
+    function test_get_book() public {
+
+        book = books.get_book();
+        assertEq(book.title, "Programming Foundry");
+        assertEq(book.author, "Pierre Tidou");
+        assertEq(book.pages, 100);
+    }
+
+    function test_updatePages() public {
+        books.update_pages(200);
+        book = books.get_book();
+        assertEq(book.pages, 200);
+    }
+}
